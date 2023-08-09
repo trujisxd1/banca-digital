21d543045e59b436d9e7521edff60020e2f69674
Gustavo M <gutamvol262@gmail.com>
2023-08-09 11:54:52 -0600

    Initial commit

---
+++ src/main/java/com/banca/digital/BancaDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.banca.digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancaDigitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BancaDigitalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/banca/digital/BancaDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banca.digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancaDigitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
